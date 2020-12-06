0850359f548101a53e987b5720433d3fd7545284
IRSMsoso <camvandertuig@gmail.com>
2020-12-06 00:45:29 -0600

    Got rid of manager and started on parsing the string for commands.

--- Module/Language.py
+++ Module/Language.py
@@ -4,13 +4,7 @@
 import discord
 import asyncio
 import pickle
-
-
-class LanguageBot(discord.Client):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-    async def on_message(self):
+from time import sleep
 
 
 class Word:
@@ -47,39 +41,45 @@ class Language:
         self.channel = None
 
 
-# This class manages the entire language as well as the discord bot.
-class LanguageManager:
-
-    def __init__(self, bot_token):
-        self.bot_token = bot_token
-        self.bot = None
-        self.is_on = False
-
-    async def run_manager(self):  # This is a blocking function that runs the entire program.
-        self.is_on = True
-        await self.start_bot()  # Start the bot.
-
-        while self.is_on:  # Main Loop
-
-
-        await self.stop_bot()
-
-    async def start_bot(self):
-        self.bot = LanguageBot()  # Construct Bot
-        print("Bot Made")
-        asyncio.get_event_loop().create_task(self.bot.start(self.bot_token))
-        print("Bot Started")
-        await self.bot.wait_until_ready()
-        print("Bot Ready")
-
-    async def stop_bot(self):
-        if self.bot is not None:
-            asyncio.get_event_loop().run_until_complete(self.bot.close())
-
-    async def shutdown_manager(self):
-        self.is_on = False
+def get_command_list(full_command):
+    original_command = full_command
+    first_command_index = original_command.find('\"')
+    if first_command_index == -1:  # There's a problem if there is no quotes in the command.
+        return None
+    command_list = [original_command[:first_command_index]]  # Create a list of commands starting with the base command.
+    original_command = original_command[first_command_index:]  # Remove that first command from the rest of the string.
+    print(command_list)
+    print(original_command)
+    while len(original_command) > 0:  # Until everything is drained form the string of commands.
+        check_same = original_command
+        command_list.append(original_command[1:original_command.find('\"', 1)])
+        original_command = original_command[original_command.find('\"', 1) + 1:]
+        if check_same == original_command:  # Check if we are stuck in a loop due to formatting errors.
+            print("Error, Something went wrong. Likely formatted incorrectly.")
+            return None
+        print(command_list)
+        print(original_command)
+        sleep(1)
 
 
-lm = LanguageManager("token")
-asyncio.get_event_loop().run_until_complete(lm.run_manager())
-print("Returned.")
+class LanguageBot(discord.Client):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.languages = []
+        self.prefix = '\\'  # What should be in front of commands. This also allows the users to change it to prevent conflict with other bots.
+        self.run_task = self.loop.create_task(self.background_tasks())
+
+    async def background_tasks(self):
+        await self.wait_until_ready()
+        pass
+
+    async def on_message(self, message):
+        print("Received Message")
+        if message.content[:len(self.prefix)] == self.prefix:  # If the beginning of the message has the proper prefix.
+            total_command = message.content[len(self.prefix):]
+            command_list = get_command_list(total_command)
+
+
+
+bot = LanguageBot()
+bot.run("")
